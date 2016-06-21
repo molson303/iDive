a1a96b0bc4858aab76b126d8fcaf959f9a9015dc
MIchael <michael.olson756@gmail.com>
2016-06-21 13:33:10 -0600

    pulling long lat from google maps api

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 var button = document.getElementById('btn')
-var HTTPRequest = new HTTPRequest();
+var httpRequest = new XMLHttpRequest();
+var userInput = document.getElementsByTagName('input')[0]
+button.addEventListener("click", function(event) {
+httpRequest.onreadystatechange = function(){
+  if(httpRequest.readyState === 4){
+    if(httpRequest.status < 400){
+      var object = JSON.parse(httpRequest.responseText)
+        for (var i = 0; i < object.results.length; i++) {
+          }
+          var long = (object.results[0].geometry.location.lng);
+          var lat = (object.results[0].geometry.location.lat);
+          console.log(long)
+        }
+      }
+    }
+
+
+httpRequest.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + userInput.value +'&key=AIzaSyA42x7FNCeCrCZZiLRep6SE2sVWjT_dDrA')
+httpRequest.send();
+})
