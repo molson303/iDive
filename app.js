b2a57546905ca7ebd087a5235692b6e2246c7b4b
MIchael <michael.olson756@gmail.com>
2016-06-21 15:40:34 -0600

    scuba api working with google maps

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 var button = document.getElementById('btn')
 var httpRequest = new XMLHttpRequest();
 var userInput = document.getElementsByTagName('input')[0]
+var long;
+var lat;
+
+
 button.addEventListener("click", function(event) {
-httpRequest.onreadystatechange = function(){
+  httpRequest.onreadystatechange = function(){
   if(httpRequest.readyState === 4){
     if(httpRequest.status < 400){
       var object = JSON.parse(httpRequest.responseText)
-        for (var i = 0; i < object.results.length; i++) {
-          }
-          var long = (object.results[0].geometry.location.lng);
-          var lat = (object.results[0].geometry.location.lat);
-          console.log(long)
+      long = (object.results[0].geometry.location.lng)
+      lat = (object.results[0].geometry.location.lat);
+      getDiveSites(lat, long);
         }
       }
     }
 
-
-httpRequest.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + userInput.value +'&key=AIzaSyA42x7FNCeCrCZZiLRep6SE2sVWjT_dDrA')
-httpRequest.send();
+    httpRequest.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?address=' + userInput.value +'&key=AIzaSyA42x7FNCeCrCZZiLRep6SE2sVWjT_dDrA')
+    httpRequest.send();
 })
+
+function getDiveSites(lat, long){
+  httpRequest.onreadystatechange = function(){
+  if(httpRequest.readyState === 4){
+    if(httpRequest.status < 400){
+      var object2 = JSON.parse(httpRequest.responseText)
+        console.log(object2)
+
+        }
+      }
+    }
+    httpRequest.open('GET', 'http://api.divesites.com/?mode=sites&lat=' + lat +'&lng=' + long + '&dist=25')
+    httpRequest.send();
+}
